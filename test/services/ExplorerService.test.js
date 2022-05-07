8a0bb61e3e88df91b720e92616d21421bfb337f0
PaolaDaft <andrepaolagd@gmail.com>
2022-05-07 16:12:23 -0500

    Explorer Service js doc and test

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,15 @@
+const ExplorerService = require('../../lib/services/ExplorerService')
+const Reader = require('../../lib/utils/Reader')
+
+describe('Unit Tests for ExplorerService class', () => {
+    test('1) Use the method get Info()', () => {
+        
+    })
+
+    test('2) Use the method get Info()jet', () => {
+        
+    });
+    test('3) Use the method get Info()', () => {
+        
+    });
+})
