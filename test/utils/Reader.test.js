4d1bb0e358e5035b32d5e97d1f6391bd2261169e
PaolaDaft <andrepaolagd@gmail.com>
2022-05-07 15:25:15 -0500

    Test for refactor de la lectura del archivo | Passed

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const Reader = require('../../lib/utils/Reader')
+
+describe('Unit Tests for Reader class', () => {
+  it('1) Part 1 Read json file', () => {
+    const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+    const rawdata = fs.readFileSync('explorers.json')
+    
+    expect(explorers).toEqual(JSON.parse(rawdata))
+  })
+})
