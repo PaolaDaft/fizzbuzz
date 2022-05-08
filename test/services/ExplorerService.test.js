7820ac1d255de4957604d8078d338ec3bd1284a5
PaolaDaft <andrepaolagd@gmail.com>
2022-05-08 06:16:15 -0500

    Test method filterByMission()

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,14 +2,26 @@ const ExplorerService = require('../../lib/services/ExplorerService')
 const Reader = require('../../lib/utils/Reader')
 
 describe('Unit Tests for ExplorerService class', () => {
-    test('1) Use the method get Info()', () => {
-        
-    })
+    test('1) Use the method filterByMission()', () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNodeMission = ExplorerService.filterByMission(explorers, "node");
 
-    test('2) Use the method get Info()jet', () => {
-        
+        explorersInNodeMission.forEach(explorer => {
+            expect(explorer.mission).toBe('node')
+        })
     });
-    test('3) Use the method get Info()', () => {
-        
+
+    /**test('2) Use the method getAmountOfExplorersByMission()jet', () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNodeMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(explorersInNodeMission).toEqual(10)
     });
+
+    test('3) Use the method getExplorersUsernamesByMission()', () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNodeMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+        expect(explorersInNodeMission).toEqual(['ajolonauta1', 'ajolonauta2', 'ajolonauta3', 'ajolonauta4', 'ajolonauta5',
+            'ajolonauta11', 'ajolonauta12', 'ajolonauta13', 'ajolonauta14', 'ajolonauta15'])
+    });*/
 })
