12cb89d55c1cc9c6e7e2bbcf80a4275ae447461b
PaolaDaft <andrepaolagd@gmail.com>
2022-05-09 08:51:36 -0500

    Ajustando archivos con el linter

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,27 +1,27 @@
-const ExplorerService = require('../../lib/services/ExplorerService')
-const Reader = require('../../lib/utils/Reader')
+const ExplorerService = require("../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/Reader");
 
-describe('Unit Tests for ExplorerService class', () => {
-    test('1) Use the method filterByMission()', () => {
+describe("Unit Tests for ExplorerService class", () => {
+    test("1) Use the method filterByMission()", () => {
         const explorers = Reader.readJsonFile("explorers.json");
         const explorersInNodeMission = ExplorerService.filterByMission(explorers, "node");
 
         explorersInNodeMission.forEach(explorer => {
-            expect(explorer.mission).toBe('node')
-        })
+            expect(explorer.mission).toBe("node");
+        });
     });
 
-    test('2) Use the method getAmountOfExplorersByMission()jet', () => {
+    test("2) Use the method getAmountOfExplorersByMission()jet", () => {
         const explorers = Reader.readJsonFile("explorers.json");
         const explorersInNodeMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-        expect(explorersInNodeMission).toEqual(10)
+        expect(explorersInNodeMission).toEqual(10);
     });
 
-    test('3) Use the method getExplorersUsernamesByMission()', () => {
+    test("3) Use the method getExplorersUsernamesByMission()", () => {
         const explorers = Reader.readJsonFile("explorers.json");
         const explorersInNodeMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-        expect(explorersInNodeMission).toEqual(['ajolonauta1', 'ajolonauta2', 'ajolonauta3', 'ajolonauta4', 'ajolonauta5',
-            'ajolonauta11', 'ajolonauta12', 'ajolonauta13', 'ajolonauta14', 'ajolonauta15'])
+        expect(explorersInNodeMission).toEqual(["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5",
+            "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]);
     });
-})
+});
