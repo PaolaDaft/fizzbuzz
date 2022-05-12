e8e187466f2fb28a793a1a93fd5240755f63ef5a
PaolaDaft <andrepaolagd@gmail.com>
2022-05-12 06:41:42 -0500

    Agregando getValidationInNumber method | test Passed

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -19,6 +19,13 @@ describe("Unit Tests for ExplorerService class", () => {
     test("3) Use the method getExplorersAmonutByMission()", () => {
         const explorersInNodeMission = ExplorerController.getExplorersAmonutByMission("node");
         expect(explorersInNodeMission).toEqual(10);
+    });
+
+    test("4) Use the method getValidationInNumber()", () => {
+        expect(ExplorerController.getValidationInNumber(1)).toEqual(1);
+        expect(ExplorerController.getValidationInNumber(3)).toBe("FIZZ");
+        expect(ExplorerController.getValidationInNumber(5)).toBe("BUZZ");
+        expect(ExplorerController.getValidationInNumber(15)).toBe("FIZZBUZZ");
 
 
     });
